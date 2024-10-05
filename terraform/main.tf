094ad3d033ae1d3d63913538427f175cadb54b99
Evita Lopez <88731596+xvitalopez@users.noreply.github.com>
2024-10-05 23:56:47 +0100

    Create main.tf
    main tf file

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-097c5c21a18dc59ea"
+  instance_type = "t3.micro"
+  
+  tags = {
+    Name = "Jenkins-Terraform-Demo"
+  }
+}
